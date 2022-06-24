a2e37f3e6ff720af3e5f43037743611f0fe473c1
Eren Arslan <77920952+ErennArslan@users.noreply.github.com>
2022-06-24 15:40:46 +0300

    Add files via upload

---
+++ PlakaKayit/PlakaKayit/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace PlakaKayit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(220, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaka Kayıt Yönetimi";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(489, 172);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Eklenecek Plaka";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(212, 201);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click_1);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(650, 377);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(106, 26);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Seçili Plakayı Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(489, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kayıtlı Plakalar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Plaka Kayıt Yönetimi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PlakaKayit/PlakaKayit/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlakaKayit
+{
+    public partial class Form1 : Form
+    {
+
+        string dosyaYolu = @"C:\Users\Arslanlar\Desktop\Plakalar.txt"; //dosya yolunu kendi bilgisayarınıza göre ayarlamalısınız
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists(dosyaYolu))
+            {
+                File.Create(dosyaYolu);
+            }
+            else
+            {
+                listBox1.Items.AddRange(File.ReadAllLines(dosyaYolu)); //dosyadaki plaka kayıtları listbox'ta görüntülendi
+            }
+            
+        }
+
+     
+        private void btnEkle_Click_1(object sender, EventArgs e)
+        {
+            string eklenecek = textBox1.Text.ToUpper(); //textbox değeri eklenecek değişkenine atandı ve karakterler büyütüldü
+            if (eklenecek != "") 
+            {
+                listBox1.Items.Add(eklenecek); 
+                textBox1.Clear(); 
+
+                string[] plakalar = listBox1.Items.OfType<string>().ToArray();
+                File.WriteAllLines(dosyaYolu, plakalar);
+            }
+      
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            DialogResult secim = new DialogResult();
+            secim = MessageBox.Show("Plaka kaydını silmek istediğinize emin misiniz?", "Plaka Silinecek", MessageBoxButtons.OKCancel);
+            
+            if(secim == DialogResult.OK)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                string[] plakalar = listBox1.Items.OfType<string>().ToArray();
+                File.WriteAllLines(dosyaYolu, plakalar);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || e.KeyChar == 8 || char.IsNumber(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
